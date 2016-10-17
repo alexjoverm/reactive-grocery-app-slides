5e4f4a5cd11a00fb6dccbf85fa1a403df847cbf0
Alex J <alexjovermorales@gmail.com>
2016-10-18 01:00:50 +0200

    Structural directives [Structural directives]

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,17 +1,20 @@
 import {Component} from '@angular/core'
 
-// 6/ Usar @Component decorator
 @Component({
   moduleId: __moduleName,   // Hack para System.js rutas relativas
   selector: 'my-app',       // Se utiliza para nombrar un component desde el HTML
   templateUrl: 'app.html',
   styleUrls: ['app.css']
 })
-// 9/ Un componente es simplemente una clase de typescript
 export class App {
   name: string
+  isVisible: boolean
+  list: any[]
 
+  // 5/ Definir isVisible, y una lista de objectos
   constructor() {
     this.name = 'Angular2'
+    this.isVisible = true
+    this.list = [{ id: 0, title: 'Paco' }, { id: 1, title: 'Pepe' }]
   }
 }

6141ce3661e9dff63d13bb50e10e37e93add9314
Alex J <alexjovermorales@gmail.com>
2016-10-23 03:13:18 +0200

    Combinar State [Combinar State]

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core'
 import { Store } from '@ngrx/store'
-import { ADD_LIST } from '../reducer'
+// Importamos ADD_LIST_ITEM action
+import { ADD_LIST, ADD_LIST_ITEM } from '../reducer'
 import { Selector } from '../selector'
+// 2/ Importar ItemsSelector y Actions
+import { ADD_ITEM } from '../items-reducer'
+import { ItemsSelector } from '../items-selector'
 
 @Component({
   moduleId: __moduleName,
@@ -10,19 +14,36 @@ import { Selector } from '../selector'
 })
 export class App implements OnInit, OnDestroy{
   lists$
+  items$
   newList = {}
 
+  // 12/ Inyectamos itemsSelector y lo usamos para obtener los items
   constructor(
     private store: Store<any>,
-    private selector: Selector
+    private selector: Selector,
+    private itemsSelector: ItemsSelector
   ) {
     this.lists$ = this.selector.getLists()
+    // 4/ Obtenemos items de la lista 0. MergeMap es como map, pero
+    // desenvuelve Observables. getItemsFromIds devuelve un Observable
+    this.items$ = this.lists$.mergeMap(lists =>
+      this.itemsSelector.getItemsFromIds(lists[0].items)
+    )
   }
 
   createList() {
     const randomId = Math.floor(Math.random() * 1000)
     const list = Object.assign({}, this.newList, { id: randomId })
     this.store.dispatch({ type: ADD_LIST, payload: list })
+  }
+
+  // 7/ Funcion simple para añadir item
+  createListItem() {
+    const randomId = Math.floor(Math.random() * 1000)
+    const item = Object.assign({}, this.newList, { id: randomId })
+    // 2/ Añadimos item, y además tenemos que actualizar la lista
+    this.store.dispatch({ type: ADD_LIST_ITEM, payload: { listId: 0, itemId: item.id } })
+    this.store.dispatch({ type: ADD_ITEM, payload: item })
   }
 
 }
--- src/selector.ts
+++ src/selector.ts
@@ -1,32 +1,17 @@
 import { Injectable } from '@angular/core'
 import { Store } from '@ngrx/store'
 
-// 30/ Un Service en Angular es simplemente una clase de
-// Typescript/ES6 que usa el decorator @Injectable
 @Injectable()
 export class Selector {
-
-  // Inyectamos el Store
   constructor(private store: Store<any>) { }
 
-  // 3/ Helper para acceder a una porción del State
   private getState() {
     return this.store.select(state => state.lists)
   }
 
-  // 5/ Obtener un array de listas, procesando los ids y entities
   public getLists() {
     return this.getState().map(state =>
       state.ids.map(id => state.entities[id])
     )
   }
-
-  // 5/ Obtener una lista en base a un id
-  public getList(id: string) {
-    // 3/ RxJS permite combinar funciones gracias a su naturaleza FP
-    return this.getListEntities()
-      .map(listEntities => listEntities[id])
-      .filter(list => !!list) // Aseguramos que exista
-  }
-
 }

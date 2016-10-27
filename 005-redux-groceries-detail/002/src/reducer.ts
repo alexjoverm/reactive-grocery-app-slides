a3cb01ce3b85b14d21dab4f599bc0179bf82e9e3
Alex J <alexjovermorales@gmail.com>
2016-10-27 17:27:49 +0200

    Updated main slide

--- 005-redux-groceries-detail/002/src/reducer.ts
+++ 005-redux-groceries-detail/002/src/reducer.ts
@@ -2,14 +2,13 @@ import { Action } from '@ngrx/store'
 
 // 6/ Mantener State sin anidar, utilizando arrays sólo para ids.
 // Estructures del estilo ids:[] con mapping a entities:{}
-export const initialState = [
+export const initialState = {
   ids: [0, 1],
   entities: {
     0: { id: 0, title: 'Paco' },
     1: { id: 1, title: 'Pepe' }
   }
-
-]
+}
 
 export const ADD_LIST = 'ADD_LIST'
 
